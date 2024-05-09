d5a3a2814bc04c34db5ac97442a71e31ea9ddd40
Peter J. Philipp <pbug44@delphinusdns.org>
2024-05-09 11:14:01 +0200

    take out hgrep for grep which is in base

--- cg4/script.sh
+++ cg4/script.sh
@@ -2,4 +2,4 @@
 
 awk '/key/ { print }' output
  
-awk '{print $1 " " $NF}' output | awk '$1 < 127 {print}' | cat -n | hgrep -e f1 -e cc -e c3 -e d2  | awk '$1 > 400 && (length($NF) < 12) {print}'
+awk '{print $1 " " $NF}' output | awk '$1 < 127 {print}' | cat -n | grep -e f1 -e cc -e c3 -e d2  | awk '$1 > 400 && (length($NF) < 12) {print}'
